b267318c6290476c1f1dff591e221ec432fd04ba
ColbyO <71828390+ColbyO@users.noreply.github.com>
2021-08-13 19:54:05 -0230

    updated login & register design

--- client/src/components/pages/LoginPage.js
+++ client/src/components/pages/LoginPage.js
@@ -1,12 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
+import { Container, Form, Button, Card, Alert } from "react-bootstrap"
 import { Link } from 'react-router-dom'
-import {Button} from '@material-ui/core'
 
 function LoginPage({history}) {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [error, setError] = useState("")
+    const [loading, setLoading] = useState(false)
 
     useEffect(()=> {
         if(localStorage.getItem("authToken")) {
@@ -33,19 +34,47 @@ function LoginPage({history}) {
                 setError("")
             }, 5000)
         }
+        setLoading(false)
     }
 
     return (
-        <div>
-        <form onSubmit={loginHandler}>
-            <h1>Login</h1>
-                <input placeholder="email" onChange={(e)=> setEmail(e.target.value)}/>
-                <input placeholder="password" onChange={(e)=> setPassword(e.target.value)}/>
-                <button  variant="contained" color="primary">Submit</button>
-                <span>Don't have an account? <Link to="/register">Register</Link></span>
-        </form>
+        <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}>
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Log In</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={loginHandler}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" onChange={(e)=> setEmail(e.target.value)} required />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" onChange={(e)=> setPassword(e.target.value)} required />
+            </Form.Group>
+            <Button disabled={loading} className="w-100 mt-3" type="submit">
+              Log In
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        Need an account? <Link to="/register">Register</Link>
+      </div>
         </div>
+      </Container>
     )
 }
 
 export default LoginPage
+
+{/* <form onSubmit={loginHandler}>
+<h1>Login</h1>
+    <input placeholder="email" onChange={(e)=> setEmail(e.target.value)}/>
+    <input placeholder="password" onChange={(e)=> setPassword(e.target.value)}/>
+    <button  variant="contained" color="primary">Submit</button>
+    <span>Don't have an account? <Link to="/register">Register</Link></span>
+</form> */}
--- client/src/components/pages/RegisterPage.js
+++ client/src/components/pages/RegisterPage.js
@@ -1,13 +1,14 @@
 import React,{useState, useEffect} from 'react'
 import axios from 'axios'
+import { Container, Form, Button, Card, Alert } from "react-bootstrap"
 import { Link } from 'react-router-dom'
-import {Button} from 'react-bootstrap'
 
 function RegisterPage({ history }) {
     const [username, setUsername] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [confirmPassword, setConfirmPassword] = useState("")
+    const [loading, setLoading] = useState(false)
 
     const [error, setError] = useState("")
 
@@ -44,24 +45,58 @@ function RegisterPage({ history }) {
                 setError("")
             }, 5000)
         }
+        setLoading(false)
     }
 
     return (
-        <div>
-            
-            <form onSubmit={registerHandler}>
-            <h1>Register</h1>
-            {error && <span>{error}</span>}
-                <input placeholder="username" value={username} onChange={(e)=> setUsername(e.target.value)}/>
-                <input placeholder="email" value={email} onChange={(e)=> setEmail(e.target.value)}/>
-                <input placeholder="password" value={password} onChange={(e)=> setPassword(e.target.value)}/>
-                <input placeholder="confirmPassword"  value={confirmPassword} onChange={(e)=> setConfirmPassword(e.target.value)}/>
-                <Button type="submit" >Submit</Button>
-
-                <span> Already have an account? <Link to="login">Login</Link></span>
-            </form>            
+        <Container
+        className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Sign Up</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={registerHandler}>
+            <Form.Group id="username">
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="username" onChange={(e)=> setUsername(e.target.value)} required />
+            </Form.Group>              
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" onChange={(e)=> setEmail(e.target.value)} required />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" onChange={(e)=> setPassword(e.target.value)} required />
+            </Form.Group>
+            <Form.Group id="password-confirm">
+              <Form.Label>Password Confirmation</Form.Label>
+              <Form.Control type="password" onChange={(e)=> setConfirmPassword(e.target.value)} required />
+            </Form.Group>
+            <Button disabled={loading} className="w-100 mt-3" type="submit">
+              Sign Up
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        Already have an account? <Link to="/login">Log In</Link>
+      </div>
         </div>
+      </Container>
     )
 }
 
 export default RegisterPage
+
+{/* <form onSubmit={registerHandler}>
+<h1>Register</h1>
+{error && <span>{error}</span>}
+    <input placeholder="username" value={username} onChange={(e)=> setUsername(e.target.value)}/>
+    <input placeholder="email" value={email} onChange={(e)=> setEmail(e.target.value)}/>
+    <input placeholder="password" value={password} onChange={(e)=> setPassword(e.target.value)}/>
+    <input placeholder="confirmPassword"  value={confirmPassword} onChange={(e)=> setConfirmPassword(e.target.value)}/>
+    <Button type="submit" >Submit</Button>
+
+    <span> Already have an account? <Link to="login">Login</Link></span>
+</form>     */}
