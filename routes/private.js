e46482bf1425bbd3e392332e41dc2c3235fb825e
ColbyO <71828390+ColbyO@users.noreply.github.com>
2021-08-15 17:23:03 -0230

    added route to get current user

--- routes/private.js
+++ routes/private.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {getPrivateData, searchPostgreSQL, searchMongoDB, logSearches, getAllLogs} = require("../controllers/private")
+const {searchPostgreSQL, searchMongoDB, logSearches, getAllLogs, getContactInfo, getCurrentUser} = require("../controllers/private")
 const { protect } = require('../middleware/auth')
 
 router.route("/search/postgresql").post(protect, searchPostgreSQL)
@@ -10,5 +10,9 @@ router.route("/search/mongodb").post(protect, searchMongoDB)
 router.route("/log/search").post(protect, logSearches)
 
 router.route("/logs").post(protect, getAllLogs)
+
+router.route("/get/contacts").post(protect, getContactInfo)
+
+router.route("/get/currentuser").get(protect, getCurrentUser)
 
 module.exports = router;
